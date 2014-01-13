5af31eda37bd1e7d8b292937fc630d7a1a31ed91
Tomita Militaru <militarutomita@gmail.com>
2014-01-13 23:13:29 +0200

    Register ZipStreamPHP

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,6 +1,8 @@
 <?php
 $classPath = t3lib_extMgm::extPath('t3download', 'Classes/');
+$resourcesPath = t3lib_extMgm::extPath('t3download', 'Resources/');
 $prefix = 'tx_t3download_';
 return array(
-    $prefix . 'service_fileservice' => $classPath . 'FileService'
+    $prefix . 'service_fileservice' => $classPath . 'FileService',
+    'ZipStream' => $resourcesPath . 'Private/ZipStreamPHP/zipstream'
 );
