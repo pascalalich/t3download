fc83edecfab38baf8e3be28e51fd11c481b83a5d
pascalalich <pascal.alich@gmx.de>
2014-06-19 21:36:25 +0200

    Backend module for managing downloads

--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,27 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+if (TYPO3_MODE === 'BE') {
+	/**
+	 * Registers a Backend Module
+	 */
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+			'TYPO3.'.$_EXTKEY,
+			'web',	 // Make module a submodule of 'web'
+			'download',	// Submodule key
+			'',						// Position
+			array(
+					'DownloadConfiguration' => 'list',
+						
+			),
+			array(
+					'access' => 'user,group',
+					'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+					'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_backend.xlf',
+			)
+	);
+}
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 	$_EXTKEY,
 	'Download',
